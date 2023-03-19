2f905bdb232adad5ab3b6ffb7c86d40a13521a9b
Remigit99 <aderemibiodun1000@gmail.com>
2023-03-19 07:50:03 +0100

    changed the link for signup to login

--- js/script.js
+++ js/script.js
@@ -23,61 +23,95 @@ fetch(apiUrl)
 
 //Switching articles and form content
 
-const steps = document.querySelectorAll('.step');
-const headerBar = document.querySelectorAll('.steps__header h6');
+const steps = Array.from(document.querySelectorAll('.step'));
+const nextBtn = document.querySelectorAll('.next');
+const cancelBtn = document.querySelector('.cancel');
+const prevBtn = document.querySelectorAll('.prev');
+const form = document.querySelector('form');
 
-console.log(headerBar)
+// console.log(steps, nextBtn, prevBtn);
 
-const formInputs = document.querySelectorAll('.input');
-
-const currentStep = 1;
-const currentBar = 0;
-
-steps.forEach( step =>{
-
-    const nextBtn = document.querySelector('.next');
-    const backBtn = document.querySelector('.back');
-
-    if(backBtn){
-        backBtn.addEventListener('click', () =>{
-            document.querySelector(`step-${currentStep}`).style.display = 'none';
-            currentStep--;
-
-            document.querySelector(`step-${currentStep}`).style.display = 'flex';
-
-            headerBar[currentBar].classList.remove('active');
-            currentBar++;
-
-        })
-
-    }
-
-    nextBtn.addEventListener('click', () =>{
-        document.querySelector(`step-${currentStep}`).style.display = 'none';
-        if(currentStep < 3 && validation() ){
-            currentStep++;
-            currentBar++;
-
-        }
-
-        document.querySelector(`step-${currentStep}`).style.display = 'flex';
-
-        headerBar[currentBar].classList.add('active');
-
-    } )
+nextBtn.forEach(button =>{ button.addEventListener('click', (e) => {
+    changeStep('next')
+    })
 })
 
-const validationForm = () =>{
-    let valid = true;
-    for(let i = 0; i < formInputs.length; i++ ){
-        if(!formInputs[i].value){
-            valid = false;
-        }else{
-            valid = true;
-        }
+prevBtn.forEach(button =>{ button.addEventListener('click', (e) => {
+    changeStep('prev')
+    })
+})
+
+
+function changeStep(btn){
+    let index = 0;
+    const show = document.querySelector('.step.show');
+    index = steps.indexOf(show);
+    steps[index].classList.remove('show');
+    if(btn === 'next'){
+        index++;
+    }else if(btn === 'prev'){
+        index--;
     }
 
-    return valid;
+    steps[index].classList.add('show');
+
 }
+
+// const headerBar = Array.from(document.querySelectorAll('.steps__header h6'));
+
+// // console.log(headerBar)
+
+// const formInputs = document.querySelectorAll('.input');
+
+// const currentStep = 1;
+// const currentBar = 0;
+
+// steps.forEach( step =>{
+
+//     const nextBtn = document.querySelector('.next');
+//     const cancelBtn = document.querySelector('.cancel');
+//     const prevBtn = document.querySelector('.prev');
+
+//     if(prevBtn){
+//         prevBtn.addEventListener('click', () =>{
+//             document.querySelector(`step-${currentStep}`).style.display = 'none';
+//             currentStep--;
+
+//             document.querySelector(`step-${currentStep}`).style.display = 'flex';
+
+//             headerBar[currentBar].classList.remove('active');
+//             currentBar++;
+
+//         })
+
+//     }
+
+//     nextBtn.addEventListener('click', () =>{
+//         document.querySelector(`step-${currentStep}`).style.display = 'none';
+//         if(currentStep < 3 && validation() ){
+//             currentStep++;
+//             currentBar++;
+
+//         }
+
+//         document.querySelector(`step-${currentStep}`).style.display = 'flex';
+
+//         headerBar[currentBar].classList.add('active');
+
+//     } )
+// })
+
+// const validationForm = () =>{
+//     let valid = true;
+//     for(let i = 0; i < formInputs.length; i++ ){
+//         if(!formInputs[i].value){
+//             valid = false;
+//         }else{
+//             valid = true;
+//         }
+//     }
+
+//     return valid;
+// }
 
 
